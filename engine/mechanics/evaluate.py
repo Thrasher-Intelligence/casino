ecf0e7654c3b010d20d91e7530294f4ccf9b810d
Jake Eaker <jacob.eaker@gmail.com>
2025-05-21 11:21:34 -0400

    fixed hand evaluation order issue.

--- engine/mechanics/evaluate.py
+++ engine/mechanics/evaluate.py
@@ -8,13 +8,13 @@ def evaluate_hand(hand):
     full_house = [rank for rank, count in counts.items() if count == 2 and rank in trips]
     four_of_a_kind = [rank for rank, count in counts.items() if count == 4]
 
-    if pairs:
-        return f"You have a pair of {pairs[0]}s!"
-    elif trips:
-        return f"You have three of a kind! {trips[0]}s!"
+    if four_of_a_kind:
+        return f"You have four of a kind! {four_of_a_kind[0]}s!"
     elif full_house:
         return f"You have a full house! {pairs[0]}s and {trips[0]}s!"
-    elif four_of_a_kind:
-        return f"You have four of a kind! {four_of_a_kind[0]}s!"
+    elif trips:
+        return f"You have three of a kind! {trips[0]}s!"
+    elif pairs:
+        return f"You have a pair of {pairs[0]}s!"
     else:
         return "No pairs found."
